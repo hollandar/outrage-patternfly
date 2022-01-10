b9ae5e07d5a28e3933b154059c2687e82a7e689b
Andrew <andrew@webefinity.com>
2022-01-11 00:21:37 +0800

    PatternflyPage: added header nav and footer sections. Added PatternflyTabs.

---
+++ Outrage.Patternfly/Components/Tabs/ITab.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Tabs
+{
+    public interface ITab
+    {
+        string TabId { get; }
+        string PanelCssClass { get; }
+        RenderFragment ChildContent { get; }
+    }
+}
