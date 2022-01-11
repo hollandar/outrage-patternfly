c244136e3ac3ca7b9b19ae9ef14e31a8b21129bc
Adrian Holland <hollandar@gmail.com>
2022-01-11 12:14:31 +1100

    Standardize 'class' handling

---
+++ Outrage.Patternfly/Components/Base/PatternflyComponentBase.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Base
+{
+    public class PatternflyComponentBase: ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public Dictionary<string, object>? AdditionalAttributes { get; set; }
+
+        [Parameter]
+        public string? Class { get; set; } = null;
+
+        protected string classes = "";
+        protected string Classes { get { return classes; } }
+        protected override void OnParametersSet()
+        {
+            AdjustClasses();
+            base.OnParametersSet();
+        }
+
+        protected virtual void AdjustClasses()
+        {
+            var classes = new HashSet<string>();
+
+            if (AdditionalAttributes?.ContainsKey("class") ?? false)
+            {
+                var elementClass = AdditionalAttributes["class"];
+                var elementClasses = (elementClass?.ToString() ?? "").Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (var @class in elementClasses) classes.Add(@class);
+            }
+
+            if (Class != null)
+            {
+                var elementClasses = (Class?.ToString() ?? "").Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                foreach (var @class in elementClasses) classes.Add(@class);
+            }
+
+            foreach (var @class in OnIntroduceClasses()) 
+                classes.Add(@class);
+
+            AdditionalAttributes?.Remove("class");
+
+            this.classes = String.Join(" ", classes);
+        }
+
+        protected virtual IEnumerable<string> OnIntroduceClasses()
+        {
+            return Enumerable.Empty<string>();
+        }
+    }
+}
