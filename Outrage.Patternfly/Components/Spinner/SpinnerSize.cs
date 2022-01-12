c0a0f03c76af4ffd47b6da3e3d06b8f2b3198660
Adrian Holland <hollandar@gmail.com>
2022-01-12 16:23:07 +1100

    Spinner

---
+++ Outrage.Patternfly/Components/Spinner/SpinnerSize.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Spinner
+{
+    public enum SpinnerSize
+    {
+        Default,
+        Small,
+        Medium,
+        Large,
+        XLarge
+    }
+}
