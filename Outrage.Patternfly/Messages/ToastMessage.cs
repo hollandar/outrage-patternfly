cf91fbac98a5f6d5f384859ba9a27f45b191cf22
Adrian Holland <hollandar@gmail.com>
2022-01-12 15:31:18 +1100

    Patternfly toaster

--- Outrage.Patternfly/Messages/ToastMessage.cs
+++ Outrage.Patternfly/Messages/ToastMessage.cs
@@ -1,15 +1,18 @@
 ﻿using Outrage.EventBus;
+using Outrage.Patternfly.Components.Alert;
 
 namespace Outrage.Patternfly.Messages
 {
 	public class ToastMessage : IMessage
     {        
-        public ToastMessage(string message, string styleClass = null)
+        public ToastMessage(string message, string? description = null, AlertColor styleClass = AlertColor.Default)
         {
             Message = message;
+            Description = description;
             StyleClass = styleClass;
         }
-        public string Message { get; }
-        public string StyleClass { get; }
+        public string Message { get; init; }
+        public string? Description { get; init; }
+        public AlertColor StyleClass { get; init; }
     }
 }
