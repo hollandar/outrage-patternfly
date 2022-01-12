71cbd000e4e2aa506c555e0cf18c3c40a95600ae
Adrian Holland <hollandar@gmail.com>
2022-01-12 16:13:34 +1100

    Change toast message paramter order

--- Outrage.Patternfly/Messages/ToastMessage.cs
+++ Outrage.Patternfly/Messages/ToastMessage.cs
@@ -5,14 +5,14 @@ namespace Outrage.Patternfly.Messages
 {
 	public class ToastMessage : IMessage
     {        
-        public ToastMessage(string message, string? description = null, AlertColor styleClass = AlertColor.Default)
+        public ToastMessage(string message, AlertColor color = AlertColor.Default, string? description = null)
         {
             Message = message;
             Description = description;
-            StyleClass = styleClass;
+            Color = color;
         }
         public string Message { get; init; }
         public string? Description { get; init; }
-        public AlertColor StyleClass { get; init; }
+        public AlertColor Color { get; init; }
     }
 }
