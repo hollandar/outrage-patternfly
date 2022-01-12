47c63d08b521bc46d428734fc34f9621b8d40d51
Adrian Holland <hollandar@gmail.com>
2022-01-12 14:28:43 +1100

    Backdrop no scroll

---
+++ Outrage.Patternfly/wwwroot/js/backdrop.ts
@@ -0,0 +1,11 @@
+﻿class BackdropHelper {
+    static apply() {
+        let body = document.body;
+        document.body.classList.add("pf-c-backdrop__open");
+    }
+
+    static unapply() {
+        let body = document.body;
+        document.body.classList.remove("pf-c-backdrop__open");
+    }
+}
