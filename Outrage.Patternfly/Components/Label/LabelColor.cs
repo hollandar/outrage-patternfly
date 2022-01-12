2558604be6c56e2160d831039489e6864ed2d282
Adrian Holland <hollandar@gmail.com>
2022-01-12 16:00:26 +1100

    Patternfly label

---
+++ Outrage.Patternfly/Components/Label/LabelColor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Label
+{
+    public enum LabelColor
+    {
+        Default,
+        Gray,
+        Blue,
+        Green,
+        Orange,
+        Red,
+        Purple,
+        Cyan
+    }
+}
