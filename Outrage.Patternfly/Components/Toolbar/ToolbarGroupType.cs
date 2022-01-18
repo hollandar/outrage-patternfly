eef49ef082f1c765f9ea82573447b23814e91250
Andrew <andrew@webefinity.com>
2022-01-19 00:48:42 +0800

    Change Label component to render icons using the Icon helper. New Toolbar component.

---
+++ Outrage.Patternfly/Components/Toolbar/ToolbarGroupType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Toolbar
+{
+    public enum ToolbarGroupType
+    {
+        none, filter, iconButton, button
+    }
+}
