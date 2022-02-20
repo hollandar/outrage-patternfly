b899d954baebcf0eb2f0975c24a3aee21c4624b3
Adrian Holland <hollandar@gmail.com>
2022-02-21 10:30:54 +1100

    Event change, modal fixes and standalone checkbox

---
+++ Outrage.Patternfly/Components/Events/EventChain.razor.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Components;
+using Outrage.EventBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Events
+{
+    public partial class EventChain
+    {
+        class ChainEventAggregator : EventAggregator
+        {
+            public ChainEventAggregator(IServiceProvider serviceProvider) : base(serviceProvider)
+            {
+                this.AddDefaultLogSubscriber();
+                this.AddDefaultExceptionSubscriber();
+            }
+        }
+
+        ChainEventAggregator eventAggregator;
+
+        ISubscriber subscriber;
+
+        [Parameter] public RenderFragment ChildContent { get; set; }
+
+        [CascadingParameter] public EventChain ParentChain { get; set; }
+
+        [Inject] IServiceProvider ServiceProvider { get; set; }
+
+        [Parameter] public EventCallback<MessageEventArgs> OnMessage { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            this.eventAggregator = new ChainEventAggregator(this.ServiceProvider);
+
+            this.subscriber = this.eventAggregator.Subscribe(this.MessageSubscriberAsync);
+            return base.OnInitializedAsync();
+        }
+
+        public async Task MessageSubscriberAsync(EventContext context, IMessage message)
+        {
+            if (this.ParentChain != null)
+                await this.ParentChain.Publish(message);
+
+            await this.InvokeAsync(async () =>
+            {
+                await this.OnMessage.InvokeAsync(new MessageEventArgs { Context = context, Message = message });
+            });
+        }
+
+        public async Task Publish(IMessage message)
+        {
+            await this.eventAggregator.PublishAsync(message);
+        }
+    }
+}
---
+++ Outrage.Patternfly/Components/Events/MessageEventArgs.cs
@@ -0,0 +1,10 @@
+﻿using Outrage.EventBus;
+
+namespace Outrage.Patternfly.Components.Events
+{
+    public class MessageEventArgs
+    {
+        public EventContext Context { get; set; }
+        public IMessage Message { get; set; }
+    }
+}
