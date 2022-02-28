63317d2cd85118041c776fc356f6fc6e1e6ed08e
Adrian Holland <hollandar@gmail.com>
2022-02-28 17:47:28 +1100

    Treeview and PatternflyActionModal

---
+++ Outrage.Patternfly/Components/Modal/ActionModalException.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Modal
+{
+    public class ActionModalException : Exception
+    {
+        public ActionModalException()
+        {
+        }
+
+        public ActionModalException(string? message) : base(message)
+        {
+        }
+
+        public ActionModalException(string? message, Exception? innerException) : base(message, innerException)
+        {
+        }
+
+        protected ActionModalException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
