1c7c8acda763804dfd90ee1d910d616aaa3f9b94
Andrew Tie <andrew@webefinity.com>
2022-03-07 10:25:01 +1100

    New Banner component. Change Toolbar OnIntroduceClasses override to use List instead of Hashset so that order of classes is preserved.

---
+++ Outrage.Patternfly/Components/DescriptionList/DescriptionListColumns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.DescriptionList
+{
+    public enum DescriptionListColumns
+    {
+        Default, Columns2, Columns3OnLg, Autofit
+    }
+}
