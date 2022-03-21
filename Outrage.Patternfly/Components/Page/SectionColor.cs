163b2bbbbdbf8573c91599a9ca5662d98ec0e308
Adrian Holland <hollandar@gmail.com>
2022-03-22 09:44:51 +1100

    PatternflyPageSection

---
+++ Outrage.Patternfly/Components/Page/SectionColor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Page
+{
+    public enum SectionColor
+    {
+        light,
+        dark_100,
+        dark_200,
+        none
+    }
+}
