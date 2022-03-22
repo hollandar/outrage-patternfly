bcad00f755d026c752d8130f5183317b2a07e769
Adrian Holland <hollandar@gmail.com>
2022-03-22 16:26:33 +1100

    Dual list selector and fix check app

---
+++ Outrage.Patternfly/Components/DualListSelector/DualListItem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.DualListSelector
+{
+    public class DualListItem<TKey>
+    {
+        TKey key;
+        string value;
+        int? count;
+
+        public DualListItem(TKey key, string value, int? count = null)
+        {
+            this.key = key;
+            this.value = value;
+            this.count = count;
+        }
+
+        public bool IsHighlighted { get; set; } = false;
+        public bool IsSelected { get; set; }
+        public bool HasCount => count.HasValue;
+        public int Count => count ?? 0;
+
+        public TKey Key => key;
+        public string Value => value;
+    }
+}
