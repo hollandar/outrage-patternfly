b5933b9c1fde80337408ae548328edd276983861
Andrew Tie <andrew@webefinity.com>
2022-03-22 15:14:04 +1100

    OverflowMenu component.

---
+++ Outrage.Patternfly/Components/OverflowMenu/OverflowMenuGroupType.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.OverflowMenu
+{
+    public enum OverflowMenuGroupType { Default, Button, IconButton }
+}
