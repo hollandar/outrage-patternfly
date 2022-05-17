013dad37cf893e2e8439ac1607393a2b3b4ce360
Adrian Holland <hollandar@gmail.com>
2022-05-17 15:36:21 +1000

    Push modals to parent page using PatternflyOnPage

---
+++ src/Outrage.Patternfly/Components/Page/PatternflyOnPage.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Page
+{
+    public class PatternflyOnPage : ComponentBase, IDisposable
+    {
+        [CascadingParameter(Name ="PatternflyParentPage")] public PatternflyPage? ParentPage { get; set; } 
+        [Parameter] public RenderFragment? ChildContent { get; set; }
+
+        public event Action OnChange = default!;
+        protected override void OnInitialized()
+        {
+            if (ParentPage != null)
+            {
+                ParentPage.SetOnPage(this);
+            }
+            else
+            {
+                throw new Exception("PatternflyOnPage must be used within a PatternflyPage context.");
+            }
+            Update();
+            base.OnInitialized();
+        }
+
+        public void Update()
+        {
+            OnChange?.Invoke();
+        }
+
+        protected override bool ShouldRender()
+        {
+            var shouldRender = base.ShouldRender();
+            if (shouldRender)
+            {
+                this.Update();
+            }
+
+            return shouldRender;
+        }
+
+
+        public void Dispose()
+        {
+            if (ParentPage?.IsSetOnPage(this) ?? false)
+            {
+                ParentPage?.SetOnPage(null);
+            }
+        }
+
+    }
+}
