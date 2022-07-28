ee1790fd78f4ef904d9a13513bb47d78c298b500
Adrian Holland <hollandar@gmail.com>
2022-07-28 13:49:59 +1000

    Fix concurrency on OnTarget

--- build/Build.cs
+++ build/Build.cs
@@ -25,7 +25,7 @@ class Build : NukeBuild
     ///   - Microsoft VSCode           https://nuke.build/vscode
 
     const string semVer = "1.0.1";
-    const string suffixVer = "rc10";
+    const string suffixVer = "rc12";
 
     public static int Main() => Execute<Build>(x => x.Compile);
 
