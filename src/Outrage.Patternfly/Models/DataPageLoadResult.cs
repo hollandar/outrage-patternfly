d0a4aecc92471be84f3ef712b90b181b699d4b1d
Adrian Holland <hollandar@gmail.com>
2023-02-24 14:09:03 +1100

    Clean up page implementations

--- src/Outrage.Patternfly/Models/DataPageLoadResult.cs
+++ src/Outrage.Patternfly/Models/DataPageLoadResult.cs
@@ -6,5 +6,13 @@
         public int PageSize { get; set; }
         public int TotalRecords { get; set; }
         public IEnumerable<TItemModel>? Items { get; set; }
+
+        public static DataPageLoadResult<TItemModel> Empty = new DataPageLoadResult<TItemModel>
+        {
+            Page = 0,
+            PageSize = 0,
+            TotalRecords = 0,
+            Items = Enumerable.Empty<TItemModel>()
+        };
     }
 }
