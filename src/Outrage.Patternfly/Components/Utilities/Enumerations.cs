12566a2cb555fb42d916bf80797b9c74150db0e0
Adrian Holland <hollandar@gmail.com>
2023-06-02 09:43:55 +1000

    TextInputGroup

---
+++ src/Outrage.Patternfly/Components/Utilities/Enumerations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Utilities
+{
+    public enum LibraryEnum { fontawesome };
+    public enum WeightEnum { light, regular, solid, duotone, brand }
+    public enum SizeEnum { xs, sm, regular, lg, x2, x3, x5, x7, x10 }
+}
