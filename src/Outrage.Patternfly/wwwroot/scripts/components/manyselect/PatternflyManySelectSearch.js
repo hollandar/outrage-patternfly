b0c4dcf8996280cdb1311cb0863d42428ecb26c9
Adrian Holland <hollandar@gmail.com>
2023-07-04 11:34:39 +1000

    Enter on many select search

---
+++ src/Outrage.Patternfly/wwwroot/scripts/components/manyselect/PatternflyManySelectSearch.js
@@ -0,0 +1,33 @@
+
+const map = new Map()
+
+export function init(searchId, searchControl) {
+    let searchElement = document.getElementById(searchId)
+
+    searchElement.addEventListener("keypress", searchKeypress);
+
+    map.set(searchId, { element: searchElement, control: searchControl })
+}
+
+function searchKeypress(event) {
+    if (event.key === "Enter") {
+        let searchId = event.target.id
+        event.preventDefault()
+        event.stopPropagation()
+
+        let target = map.get(searchId);
+        target.control.invokeMethod("SearchItems")
+    }
+}
+
+export function getSearchText(searchId) {
+    return map.get(searchId).element.value;
+}
+
+export function clearSearch(searchId) {
+    map.get(searchId).element.value = ""
+}
+
+export function dispose(searchId) {
+    map.delete(searchId);
+}
