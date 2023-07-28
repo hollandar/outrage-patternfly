8cff03b76a9cc5cb777d9d8db3907ada55a794a9
Adrian Holland <hollandar@gmail.com>
2023-07-28 13:32:15 +1000

    Multiple select toggle group

---
+++ src/Outrage.Patternfly/Components/ToggleGroup/PatternflyToggleGroupBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Components;
+using Outrage.Patternfly.Components.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.ToggleGroup
+{
+    public abstract class PatternflyToggleGroupBase<TValueType> : PatternflyComponentBase
+    {
+        List<PatternflyToggleGroupItem<TValueType>> toggleGroupItems = new();
+
+        public PatternflyToggleGroupBase(): base()
+        {
+        }
+
+        public void AddToggleGroupItem(PatternflyToggleGroupItem<TValueType> group)
+        {
+            this.toggleGroupItems.Add(group);
+        }
+
+        public abstract Task SetValue(TValueType? value);
+        public abstract bool Contains(TValueType value);
+    }
+}
