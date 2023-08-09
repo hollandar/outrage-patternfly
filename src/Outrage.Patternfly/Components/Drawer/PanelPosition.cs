90b8c9c9a34b6e5153d57750e367727573704fbe
Adrian Holland <hollandar@gmail.com>
2023-08-09 14:17:53 +1000

    Patternfly Drawer

---
+++ src/Outrage.Patternfly/Components/Drawer/PanelPosition.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Outrage.Patternfly.Components.Drawer
+{
+    public enum PanelPosition
+    {
+        None,
+
+        Left,
+        Right,
+        Static,
+        Bottom,
+        Inline,
+    }
+}
